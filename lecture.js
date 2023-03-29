135362153be13ff4cae64ae27ad5492bbf2e6be7
akshat rait <akshatrait@computingforall.org>
2023-03-28 17:35:17 -0700

    finished student activity

--- lecture.js
+++ lecture.js
@@ -6,16 +6,43 @@ Make sure to code-along during lecture.
 
 // 1. Link the script.js file from the HTML
 
+    
 // 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
+//camelCaseLooksLikeThis
+
 
 // 3. Creating Variables + Data Types
+    let mySavings = 3;
 
+    mySavings = "This is a sentence";
+    
+    alert(mySavings);
+    console.log(mySavings);
+
+    /*
+            String= Text
+            Number= Number
+            Bigint= Reallt Big Number
+            Boolean= True or False
+            Undefined= Value is undefined until you set one
+            Null= There is no value
+            Symbol= Symbol
+
+    */
+   //3 Object Data Types
+   /*
+   object= unindexed list of key value pairs
+   array= indexed list of values
+   date= date
+   */
 // 4. Creating Functions (+ Event Listeners?)
 
 // 5. Strings
-
+let color = "Yellow";
+let lastName = "Johnson";
 // 6. Numbers
-
+let length = 16;
+let weight = 7.5;
 // 7. Arrays and Loops
 
 // 8. If Statements/Conditionals
--- student.js
+++ student.js
@@ -4,14 +4,20 @@ Complete the following numbered tasks:
 */
 
 // 1. Declare 2 variables below:
-
+    let cars;
+    let engine;
+    let goodEngine;
 
 // Initialize 3 variables:
 
     // 2. Assign one to the type Number below:
-
+    engine = "v8";
 
     // 3. Assign one to the type Boolean below:
-    
+    engine= true;
 
     // 4. Assign one to the type String below:
+    cars = 9;
+
+console.log(cars);
+console.log(engine);
